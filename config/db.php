34eeaca78658678a27c7bcacb20712143af73bcc
Javier <javierjesus.ramirezvelazquez_29@alumno.academiadf.com>
2019-08-22 16:18:53 +0100

    Definiendo vars

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => getenv('DATABASE_URL') || 'mysql:host=localhost;dbname=balancer',
-    'username' => getenv('DATABASE_URL') ? null : 'root',
-    'password' => '',
+    'dsn' =>  getenv('DATABASE_NAME') ? "pgsql:host=". getenv('DATABASE_HOST') . ";port=". getenv('DATABASE_PORT') . ";dbname=" . getenv('DATABASE_NAME') : 'mysql:host=localhost;dbname=balancer',
+    'username' => getenv('DATABASE_USERNAME') || 'root',
+    'password' => getenv('DATABASE_PASSWORD') || '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
